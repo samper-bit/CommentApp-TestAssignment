821e5a6fbff32873ee2234c7aabb9beef0280d7f
User <yarik.sawchuck3102004@gmail.com>
2025-02-10 22:18:50 +0200

    Develop Abstractions

---
+++ src/CommentApp.Backend/CommentApp.Domain/Abstractions/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace CommentApp.Domain.Abstractions;
+public class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; } = default!;
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? LastModified { get; set; }
+}
---
+++ src/CommentApp.Backend/CommentApp.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CommentApp.Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? LastModified { get; set; }
+}
