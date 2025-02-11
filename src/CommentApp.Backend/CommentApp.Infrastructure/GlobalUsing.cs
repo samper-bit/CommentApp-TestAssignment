4d6ddb0e04e9a76063aee0bb4c60b36b62caa8c6
User <yarik.sawchuck3102004@gmail.com>
2025-02-11 16:21:52 +0200

    Develop EF Core DBContext Object, Mapping Objects to EF Entities

---
+++ src/CommentApp.Backend/CommentApp.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using CommentApp.Domain.Models;
+global using CommentApp.Domain.ValueObjects;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.Reflection;
+global using File = CommentApp.Domain.Models.File;
