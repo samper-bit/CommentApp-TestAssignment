ab932a254e319781bce4769229f013c0c6f9f9bd
User <yarik.sawchuck3102004@gmail.com>
2025-02-11 16:56:28 +0200

    Auto Migrate EF Core Entities to SQL Server when Application Startup with Coding

--- src/CommentApp.Backend/CommentApp.API/Program.cs
+++ src/CommentApp.Backend/CommentApp.API/Program.cs
@@ -1,6 +1,7 @@
 using CommentApp.API;
 using CommentApp.Application;
 using CommentApp.Infrastructure;
+using CommentApp.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,5 +14,9 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+    await app.InitialiseDatabaseAsync();
 
 app.Run();
---
+++ src/CommentApp.Backend/CommentApp.Infrastructure/Data/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+namespace CommentApp.Infrastructure.Data.Extensions;
+public static class DatabaseExtensions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+    }
+}
