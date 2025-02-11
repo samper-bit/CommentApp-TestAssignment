bca9b872c203c1088e608feb0b797855ef0487be
User <yarik.sawchuck3102004@gmail.com>
2025-02-11 17:08:37 +0200

    Fix File.CommentId Property

--- src/CommentApp.Backend/CommentApp.Infrastructure/Data/Configurations/FileConfiguration.cs
+++ src/CommentApp.Backend/CommentApp.Infrastructure/Data/Configurations/FileConfiguration.cs
@@ -10,8 +10,8 @@ public class FileConfiguration : IEntityTypeConfiguration<File>
             dbId => FileId.Of(dbId));
 
         builder.Property(c => c.CommentId).HasConversion(
-            commentId => commentId.Value,
-            dbId => CommentId.Of(dbId));
+            commentId => commentId == null ? (Guid?)null : commentId.Value,
+            dbId => dbId == null ? null : CommentId.Of(dbId.Value));
 
         builder.Property(f => f.FilePath)
             .IsRequired()
