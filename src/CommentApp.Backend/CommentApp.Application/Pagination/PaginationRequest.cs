cc078b39105127269e0e7a80dcba13e6c219d83b
User <yarik.sawchuck3102004@gmail.com>
2025-02-12 18:21:06 +0200

    Develop Pagination, Sorting, Filtering

---
+++ src/CommentApp.Backend/CommentApp.Application/Pagination/PaginationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace CommentApp.Application.Pagination;
+
+public record PaginationRequest(
+    int PageIndex = 0,
+    int PageSize = 25,
+    string SortField = "CreatedAt",
+    bool Ascending = false);
