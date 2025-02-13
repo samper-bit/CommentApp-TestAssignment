7f8cc3e9c43edd2cf7523483c4d9beb8cc10a17a
User <yarik.sawchuck3102004@gmail.com>
2025-02-13 14:59:30 +0200

    Register Carter service in DI

--- src/CommentApp.Backend/CommentApp.API/DependencyInjection.cs
+++ src/CommentApp.Backend/CommentApp.API/DependencyInjection.cs
@@ -1,13 +1,17 @@
-﻿namespace CommentApp.API;
+﻿using Carter;
+
+namespace CommentApp.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        services.AddCarter();
         return services;
     }
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapCarter();
         return app;
     }
 }
