3a52a0b27f916b48d268264834feee336910bdc2
User <yarik.sawchuck3102004@gmail.com>
2025-02-15 23:52:00 +0200

    Fixing InitialData

--- src/CommentApp.Backend/CommentApp.Infrastructure/Data/Extensions/InitialData.cs
+++ src/CommentApp.Backend/CommentApp.Infrastructure/Data/Extensions/InitialData.cs
@@ -7,7 +7,7 @@ internal class InitialData
         {
             // Creating comment1 with image file
             var comment1 = Comment.Create(CommentId.Of(new Guid("20e8daea-3f42-4c21-80f1-c6b7c7aaa542")), "John", "john@gmail.com", "https://john.homepage.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem ullamcorper orci tincidunt, ut fringilla risus lacinia. Vivamus nec sodales elit. Nam malesuada enim in risus laoreet pellentesque.", null, null);
-            var imageFile = File.Create(FileId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), CommentId.Of(new Guid("20e8daea-3f42-4c21-80f1-c6b7c7aaa542")), "image.jpg");
+            var imageFile = File.Create(FileId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), CommentId.Of(new Guid("20e8daea-3f42-4c21-80f1-c6b7c7aaa542")), ".jpg");
             comment1.AddFile(imageFile);
 
             // Adding child comments to comment1
@@ -22,7 +22,7 @@ internal class InitialData
 
             // Creating comment2 with text file
             var comment2 = Comment.Create(CommentId.Of(new Guid("eec28f1d-8e6f-4b0d-bc69-aaf765db6975")), "Bob", "bob@gmail.com", "https://bob.homepage.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem ullamcorper orci tincidunt, ut fringilla risus lacinia. Vivamus nec sodales elit. Nam malesuada enim in risus laoreet pellentesque.", null, null);
-            var textFile = File.Create(FileId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), CommentId.Of(new Guid("eec28f1d-8e6f-4b0d-bc69-aaf765db6975")), "text.txt");
+            var textFile = File.Create(FileId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), CommentId.Of(new Guid("eec28f1d-8e6f-4b0d-bc69-aaf765db6975")), ".txt");
             comment2.AddFile(textFile);
 
             return new List<Comment> { comment1, comment2, childComment1, childComment2, childCommentForChildComment1 };
