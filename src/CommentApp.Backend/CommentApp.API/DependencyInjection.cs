c9fe80c892546cb8584ef4a1bde0901f538ffca4
User <yarik.sawchuck3102004@gmail.com>
2025-02-16 00:06:58 +0200

    Adding Cors Policy in DI

--- src/CommentApp.Backend/CommentApp.API/DependencyInjection.cs
+++ src/CommentApp.Backend/CommentApp.API/DependencyInjection.cs
@@ -5,10 +5,18 @@ public static class DependencyInjection
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         services.AddCarter();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins", builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
         return services;
     }
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.UseCors("AllowAllOrigins");
         app.MapCarter();
         return app;
     }
